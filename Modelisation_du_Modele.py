83690676117321fafdb8bd2250f3a01ecb2adb80
Evgeni1494 <yoni1@hotmail.fr>
2023-03-17 16:05:53 +0100

    DJBTEAM-8 Meilleur model pour le projet SCRUM

---
+++ Modelisation_du_Modele.py
@@ -0,0 +1,42 @@
+
+def Model():
+    i = 0
+    modele = 1
+    for i in modele:
+        if i == 0:
+            modele = 'sait tout faire'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
